122cfd3de9cff56767ed387fdfb66c5a20c60a67
DrSh1ny <DrSh1ny@users.noreply.github.com>
2020-03-13 19:07:21 +0000

    a

---
+++ ficha4/aula1.py
@@ -0,0 +1,49 @@
+
+
+separadores=[",",";","."]
+
+lista=[]
+
+inp=input()
+
+if(inp=="TEXTO"):
+    linha=input()
+    while(linha!="FIM."):
+        linha=linha.lower()
+        print(linha)
+        for elem in separadores:
+            linha=linha.replace(elem," ")
+        linha=linha.split()
+        lista.append(linha)
+        linha=input()
+    print("GUARDADO.")
+
+
+
+inp=input()
+while(inp):
+    inp=inp.split()
+
+    if(inp[0]=="LINHAS"):
+        palavra=inp[1]
+        countLinhas=[]
+        for i in range(len(lista)):
+            if(palavra in lista[i]):
+                countLinhas.append(i)
+        if(len(countLinhas)>0):
+            for i in countLinhas:
+                print(i,end=" ")
+            print("")
+        else:
+            print("-1")
+
+    if(inp[0]=="ASSOC"):
+        palavra=inp[1]
+        numLinha=inp[2]
+        
+        if(palavra in lista[int(numLinha)]):
+            print("ENCONTRADA.")
+        else:
+            print("NAO ENCONTRADA.")
+    inp=input()
+    
